776b61b9cf5e8c25cf51feed2c82cf1dee8a0480
uziel78 <rune.husa78@gmail.com>
2021-10-19 18:00:07 +0200

    basic project complete.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Todo from "./components/Todo";
+
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="Course" />
+      <Todo text="Course 2" />
+    </div>
+  );
 }
 
 export default App;
